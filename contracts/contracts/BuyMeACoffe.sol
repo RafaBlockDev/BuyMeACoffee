48953b81ef0694192d9bfe9370166166e2e3657a
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-06-13 23:55:15 -0500

    Adding smart contract advac

---
+++ contracts/contracts/BuyMeACoffe.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.log";
+
+contract Coffee {
+    uint256 totalCoffee;
+
+    address payable public rafa;
+
+    event NewCoffee(
+        address indexed from,
+        uint256 timestamp,
+        string message,
+        string name
+    );
+
+    struct Coffee {
+        address giver;
+        string message;
+        string name;
+        uint256 timestamp;
+    }
+
+    Coffee[] coffee;
+
+    constructor() payable {
+        console.log("Hello! Buy me a coffee smart contract");
+
+        rafa = payable(msg.sender);
+    }
+
+    function getAllCoffee() public view returns (Coffee[] memory) {
+        return coffee;
+    }
+
+    function getTotalCoffee() public view returns (uint256) {
+        console.log("We have %d total coffee received ", totalCoffee);
+        return totalCoffee;
+    }
+
+    function buyCofee(
+        string memory _message,
+        string memory _name,
+        uint256 _payAmount
+    ) public payable {
+        uint256 cost = 0.001 ether;
+        require(_payAmount <= cost, "Insuficient Matic provided");
+
+        totalCoffee += 1;
+        console.log("%s has just sent a coffee")
+    }
+}
